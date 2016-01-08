aeb00014e21ea6f0402138badd949f9083dda79f
Roman Ivanov <rom6190@yandex.ru>
2016-01-08 18:13:12 +0600

    InitialCommit

---
+++ JsonSerializer/JsonSerializerTests/JsonSerializerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentAssertions;
+using Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace JsonSerializerTests
+{
+    [TestClass]
+    public class JsonSerializerTests
+    {
+        private JsonSerializer serializer = new JsonSerializer();
+
+        [Test]
+        public void StringSerializationTest()
+        {
+            serializer.Serialize("Test").Should().Be("\"Test\"");
+        }
+
+        [Test]
+        public void IntSerializationTest()
+        {
+            serializer.Serialize(5).Should().Be("5");
+        }
+
+        [Test]
+        public void NullSerializationTest()
+        {
+            serializer.Serialize(null).Should().Be("null");
+        }
+
+        [Test]
+        public void IntArraySerializerTest()
+        {
+            serializer.Serialize(new int[] { 3, 4, 6 }).Should().Be("[3,4,6]");
+        }
+    }
+}
